c2e9377b92ad36693ec1133171f459ab9d2854c3
Sam Ritchie <samritchie@google.com>
2020-01-09 14:24:33 -0700

    add versions
    Change-Id: I65a25bc95b036a704a98002e1546b4bdca382ee7

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
-    readme = f.read()
+  readme = f.read()
+
+
+def with_versioneer(f, default=None):
+  """Attempts to execute the supplied single-arg function by passing it
+versioneer if available; else, returns the default.
+
+  """
+  try:
+    import versioneer
+    return f(versioneer)
+  except ModuleNotFoundError:
+    return default
+
 
 REQUIRED_PACKAGES = []
 setup(
-  name='uv',
-  version='0.1.0',
-  description='Shared tooling for Blueshift research',
-  long_description=readme,
-  author='Blueshift Team',
-  author_email='samritchie@google.com',
-  url='https://team.git.corp.google.com/blueshift/uv',
-  packages=find_packages(exclude=('tests', 'docs')),
-  install_requires=REQUIRED_PACKAGES,
-  include_package_data=True,
+    name='uv',
+    version=with_versioneer(lambda v: v.get_version()),
+    cmdclass=with_versioneer(lambda v: v.get_cmdclass(), {}),
+    description='Shared tooling for Blueshift research',
+    long_description=readme,
+    author='Blueshift Team',
+    author_email='samritchie@google.com',
+    url='https://team.git.corp.google.com/blueshift/uv',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
 )
